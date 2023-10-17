27f57f7caf099efd32106122fbb8683fd9428cdd
PINGWINbl4 <mikmez01@gmail.com>
2023-10-17 11:23:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http')
+app.use(express.json());
+const {writeToLog} = require('./utils')
+app.post("/:code", async (req, res, next)=>{
+    try{
+        console.log(req.body)
+        writeToLog(req.body.data, req.params.code)
+        res.json("done")
+    }
+    catch(err){
+        next(err)
+    }
+  })
+  
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+      status: err.status || 500,
+      message: err.message,
+    });
+  });
+  
+  const PORT = process.env.APP_PORT || 5282;
+  const HOST = process.env.APP_HOST || "localhost"
+  app.listen(PORT, HOST, () => console.log(`🚀 @ http://${HOST}:${PORT}`));
---
+++ utils.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require('@prisma/client');
+
+const db = new PrismaClient();
+
+async function writeToLog(data, code){
+
+    const logCode = await db.EventCode.findUnique({
+        where:{
+            code: Number(code)
+        }
+    })
+    data.message = logCode.description
+    if (logCode.description.indexOf('{sensorName}') && data.sensorName !== undefined){
+        data.message = logCode.description.replace('{sensorName}', data.sensorName);
+        logCode.description = data.message
+        delete data.sensorName
+    }
+    if (logCode.description.indexOf('{shelldueName}') && data.shelldueName !== undefined){
+        data.message = logCode.description.replace('{shelldueName}', data.shelldueName);
+        logCode.description = data.message
+        delete data.shelldueName
+    }
+    data.codeId = logCode.id
+    console.log(data)
+    const eLog = await db.EventLog.create({
+        data:data
+    })
+    return eLog
+}
+
+module.exports = {
+    writeToLog
+}
