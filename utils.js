cd971f1243b08d1aa35c9396629a516453fcc437
PINGWINbl4 <mikmez01@gmail.com>
2023-11-21 14:46:42 +0500

    cleaner and number fields added into log message

--- utils.js
+++ utils.js
@@ -2,31 +2,36 @@ const { PrismaClient } = require('@prisma/client');
 
 const db = new PrismaClient();
 
-async function writeToLog(data, code){
+async function writeToLog(dataToLog, code){
     const logCode = await db.EventCode.findUnique({
         where:{
             code: Number(code)
         }
     })
-    data.message = logCode.description
-    if (logCode.description.indexOf('{sensorName}') && data.sensorName !== undefined){
-        data.message = logCode.description.replace('{sensorName}', data.sensorName);
-        logCode.description = data.message
-        delete data.sensorName
-    }
-    if (logCode.description.indexOf('{shelldueName}') && data.shelldueName !== undefined){
-        data.message = logCode.description.replace('{shelldueName}', data.shelldueName);
-        logCode.description = data.message
-        delete data.shelldueName
-    }
-    if (logCode.description.indexOf('{roomName}') && data.roomName !== undefined){
-        data.message = logCode.description.replace('{roomName}', data.roomName);
-        logCode.description = data.message
-        delete data.roomName
-    }
-    data.codeId = logCode.id
+    const lastSensorData = await db.data.findUnique({
+        where:{
+            id: dataToLog.dataId
+        }
+    })
+    dataToLog.message = logCode.description
+
+    
+    const substringsToChange = ['sensorName','shelldueName','roomName']
+    substringsToChange.forEach(substring =>{
+        if (logCode.description.indexOf(`{${substring}}`) && dataToLog[substring] !== undefined){
+            logCode.description = logCode.description.replace(`{${substring}}`, dataToLog[substring]);
+            delete dataToLog[substring]
+        }
+    })
+    lastSensorData.value.forEach(field=>{
+        if (logCode.description.indexOf(`{${field}}`)){
+            dataToLog.message = logCode.description.replace(`{${field}}`, lastSensorData.value[field]);
+            logCode.description = dataToLog.message
+        }
+    })
+    dataToLog.codeId = logCode.id
     const eLog = await db.EventLog.create({
-        data:data
+        data:dataToLog
     })
     return eLog
 }
